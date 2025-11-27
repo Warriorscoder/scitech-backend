67e6310399be9c835146abcaa7c9f4c5ff860d6a
Aniket Bharane <codeaniket123@gmail.com>
2025-11-27 13:35:18 +0530

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: 'https://scitech-frontend-b1b8m44vh-warriors-projects-7af56d98.vercel.app/', 
-    credentials: true,
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   await app.listen(process.env.PORT ?? 5000);
